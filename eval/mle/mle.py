a487a3b705103bf1d404ebd2fff83cb3de82d7c3
rennan <rennancl@gmail.com>
2024-06-03 10:27:53 -0300

    fix requirements

--- eval/mle/mle.py
+++ eval/mle/mle.py
@@ -204,7 +204,7 @@ def feat_transform(data, info, label_encoder = None, encoders = None, cmax = Non
             if encoder:
                 feature = encoder.transform(col)
             else:
-                encoder = OneHotEncoder(sparse=False, handle_unknown='ignore')
+                encoder = OneHotEncoder(sparse_output=False, handle_unknown='ignore')
                 encoders[idx] = encoder
                 feature = encoder.fit_transform(col)
                 
@@ -294,7 +294,7 @@ class FeatureMaker:
                     if encoder:
                         feature = encoder.transform(col)
                     else:
-                        encoder = OneHotEncoder(sparse=False, handle_unknown='ignore')
+                        encoder = OneHotEncoder(sparse_output=False, handle_unknown='ignore')
                         self.encoders[index] = encoder
                         feature = encoder.fit_transform(col)
 
